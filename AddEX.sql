98105ae662de95918f182b088ec6ded481bee6c1
DTH235774-NguyenTriThien <jackmian2008@gmail.com>
2025-10-08 12:02:02 +0700

    Them du lieu mau cho db

---
+++ AddEX.sql
@@ -0,0 +1,15 @@
+﻿-- DỮ LIỆU MẪU
+INSERT INTO NhanVien VALUES
+('NV001', N'Nguyễn Văn A', N'Nam', '2000-05-01', N'Phục vụ', 5000000, N'Đang làm'),
+('NV002', N'Trần Thị B', N'Nữ', '1999-09-10', N'Thu ngân', 6000000, N'Đang làm');
+
+INSERT INTO SanPham VALUES
+('CF001', N'Cà phê đen', N'Cà phê', 20000, N'Còn bán'),
+('CF002', N'Cà phê sữa', N'Cà phê', 25000, N'Còn bán'),
+('TS001', N'Trà sữa trân châu', N'Trà sữa', 35000, N'Còn bán'),
+('KE001', N'Bánh kem', N'Thức ăn', 40000, N'Còn bán');
+
+INSERT INTO KhachHang VALUES
+('KH001', N'Lê Minh Tuấn', '0912345678', 10),
+('KH002', N'Ngô Thảo Vy', '0988777666', 5);
+GO

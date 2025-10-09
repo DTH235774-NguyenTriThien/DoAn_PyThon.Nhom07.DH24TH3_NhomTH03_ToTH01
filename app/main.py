96147398d3f747eeac1eadd3a038dc12f04c8222
DTH235774-NguyenTriThien <jackmian2008@gmail.com>
2025-10-09 13:39:50 +0700

    Thêm cửa sổ chính cho giao diện chung

---
+++ app/main.py
@@ -0,0 +1,15 @@
+# app/main.py
+import tkinter as tk
+from app.ui.login_frame import show_login
+
+def main():
+    root = tk.Tk()
+    root.title("Phần mềm quản lý quán cà phê")
+    root.geometry("900x600")
+    root.configure(bg="#f5f0e1")
+
+    show_login(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()

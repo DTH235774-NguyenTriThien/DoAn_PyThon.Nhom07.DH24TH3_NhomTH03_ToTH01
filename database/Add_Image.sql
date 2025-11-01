315185da47c52a76aaa450ca02ae8cc28f66e40f
DTH235774-NguyenTriThien <jackmian2008@gmail.com>
2025-11-01 19:17:58 +0700

    Thêm ảnh sản phẩm vào dtb

---
+++ database/Add_Image.sql
@@ -0,0 +1,31 @@
+﻿SELECT * FROM NguyenLieu
+SELECT * FROM KhachHang
+SELECT * from HoaDon
+select * from ChiTietHoaDon
+select * from congthuc
+select * from SanPham	
+
+-- 1. Thêm cột mới để lưu đường dẫn ảnh
+ALTER TABLE SanPham
+ADD ImagePath NVARCHAR(255) NULL;
+GO
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/caphe_sua.png'
+WHERE TenSP = N'Cà Phê Sữa';
+GO
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/caphe_den.png'
+WHERE TenSP = N'Cà Phê Đen';
+GO
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/bac_xiu.png'
+WHERE TenSP = N'Bạc Xỉu';
+GO
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/tradao_camxa.png'
+WHERE TenSP = N'Trà Đào Cam Sả';
+GO

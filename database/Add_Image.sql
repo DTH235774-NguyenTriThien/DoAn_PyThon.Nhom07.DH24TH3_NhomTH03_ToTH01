a07a48c29f23dd52eab0fa44195c62cfc74f1b49
DTH235774-NguyenTriThien <jackmian2008@gmail.com>
2025-11-02 14:15:22 +0700

    Thêm file ảnh vào bảng sản phẩm

--- database/Add_Image.sql
+++ database/Add_Image.sql
@@ -29,3 +29,108 @@ UPDATE SanPham
 SET ImagePath = 'app/assets/products/tradao_camxa.png'
 WHERE TenSP = N'Trà Đào Cam Sả';
 GO
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/tra_vai.png'
+WHERE TenSP = N'Trà Vải';
+GO
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/banh_croissant.png'
+WHERE TenSP = N'Bánh Croissant';
+GO
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/banh_tiramisu.png'
+WHERE TenSP = N'Bánh Tiramisu';
+GO
+
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/cam_ep.png'
+WHERE TenSP = N'Nước Cam Ép';
+GO
+
+
+
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/cappuccino.png'
+WHERE TenSP = N'Cappuccino';
+GO
+
+
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/espresso.png'
+WHERE TenSP = N'Espresso';
+GO
+
+
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/tra_sen.png'
+WHERE TenSP = N'Trà Sen';
+GO
+
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/latte.png'
+WHERE TenSP = N'Latte';
+GO
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/caphe_cotdua.png'
+WHERE TenSP = N'Cà Phê Cốt Dừa';
+GO
+
+
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/cookie_daxay.png'
+WHERE TenSP = N'Cookie Đá Xay';
+GO
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/hat_huongduong.png'
+WHERE TenSP = N'Hạt hướng dương';
+GO
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/nuoc_chanh.png'
+WHERE TenSP = N'Latte';
+GO
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/latte.png'
+WHERE TenSP = N'Nước Chanh';
+GO
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/sinhto_bo.png'
+WHERE TenSP = N'Sinh tố Bơ';
+GO
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/sinhto_xoai.png'
+WHERE TenSP = N'Sinh tố Xoài';
+GO
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/tra_olong.png'
+WHERE TenSP = N'Trà Ô Long';
+GO
+
+
+
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/tragung_matong.png'
+WHERE TenSP = N'Trà Gừng Mật Ong';
+GO
+
+
+UPDATE SanPham
+SET ImagePath = 'app/assets/products/trasua_mochi.png'
+WHERE TenSP = N'Trà Sữa Mochi';
+GO
